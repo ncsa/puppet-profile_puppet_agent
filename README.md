0ce669816557483d75f08968a37e9d214f36a6ac
Bill Glick <bglick@illinois.edu>
2021-04-08 16:48:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# profile_puppet_agent
+
+[![pdk-validate](https://github.com/ncsa/puppet-profile_puppet_agent/actions/workflows/pdk-validate.yml/badge.svg)](https://github.com/ncsa/puppet-profile_puppet_agent/actions/workflows/pdk-validate.yml)
+[![yamllint](https://github.com/ncsa/puppet-profile_puppet_agent/actions/workflows/yamllint.yml/badge.svg)](https://github.com/ncsa/puppet-profile_puppet_agent/actions/workflows/yamllint.yml)
+
+NCSA Common Puppet Profiles - base pam access config
+
+## Dependencies
+n/a
+
+## Reference
+
+[REFERENCE.md](REFERENCE.md)
---
+++ manifests/init.pp
@@ -0,0 +1,53 @@
+# @summary configuration of puppet client
+#
+# @param absent_packages
+#   Array of package names to ensure absent
+#
+# @param packages
+#   Array of package names to install
+#
+# @param repo_rpm_url
+#   String of URL for puppet repo package
+#
+# @param service_enabled
+#   Boolean to determine if the puppet agent service is enabled
+#
+# @param service_name
+#   String of the name of the puppet agent service
+#
+# @param service_running
+#   Boolean to determine if the puppet agent service is ensured running
+#
+# @example
+#   include profile_puppet_agent
+class profile_puppet_agent
+(
+  Array[ String ] $absent_packages,
+  Array[ String ] $packages,
+  String          $repo_rpm_url,
+  Boolean         $service_enabled,
+  String          $service_name,
+  Boolean         $service_running,
+) {
+
+  $absent_packages_defaults = {
+    ensure  => purged,
+  }
+  # Ensure the resources
+  ensure_packages( $absent_packages, $absent_packages_defaults )
+
+  package { $repo_rpm_url:
+    provider => 'rpm',
+  }
+
+  $packages_defaults = {
+    require => Package[$repo_rpm_url],
+  }
+  ensure_packages( $packages, $packages_defaults )
+
+  service { $service_name:
+    ensure => $service_running,
+    enable => $service_enabled,
+  }
+
+}
